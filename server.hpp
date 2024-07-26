bb5c98f564dea956eb7321f3f5574ca99b04b4a8
Marwan Sekhsoukh <msekhsou@e3r9p8.1337.ma>
2024-07-26 18:37:26 +0100

    irc

---
+++ server.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/22 13:51:25 by msekhsou          #+#    #+#             */
+/*   Updated: 2024/07/25 13:06:37 by msekhsou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#include <iostream>
+#include <vector>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <poll.h>
+#include <csignal>
+
+
+class   Server
+{
+    
+};
+
+#endif
